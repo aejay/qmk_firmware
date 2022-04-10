9de83a4a7c7c44e0c92dfff1df3af4d61a2bc9cc
Aejay Goehring <aejay.goehring@gmail.com>
2022-04-10 14:06:47 -0500

    Adding webusb pair key

--- keyboards/moonlander/keymaps/aejay/keymap.c
+++ keyboards/moonlander/keymaps/aejay/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, _______,           _______,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, GOG_MUT,
         KC_BSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, _______,           _______,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, MO(DEU), RGB_TOG, KC_LGUI, KC_LALT,          TT(ARR),           TT(NMP),          KC_RALT,  KC_APP, RGB_MOD, XXXXXXX, KC_RCTRL,
+        KC_LCTL, MO(DEU), WEBUSB_PAIR, KC_LGUI, KC_LALT,          TT(ARR),           TT(NMP),          KC_RALT,  KC_APP, RGB_MOD, XXXXXXX, KC_RCTRL,
                                                 KC_SPC,  MO(NUM), RDP_MIN,           GOG_MUT,  MO(SYM), KC_ENT
     ),
 
